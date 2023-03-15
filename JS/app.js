e69fa422e5ea6ff0c07b3d008e48f00391ab042d
Zaylin <chaveszay30@gmail.com>
2023-03-15 12:05:09 -0600

    Curso de validaciones Finalizado

---
+++ JS/app.js
@@ -0,0 +1,7 @@
+import { valida } from "./validacionesMejorado.js";
+ const inputs= document.querySelectorAll('input');
+ inputs.forEach(input => {
+    input.addEventListener("blur", (event)=>{
+        valida(event.target);
+    })
+ });
---
+++ JS/validaciones.js
@@ -0,0 +1,21 @@
+const inputNacimiento=document.querySelector("#birth");
+//evento se dispara cuando un elemento ha perdido el foco.
+inputNacimiento.addEventListener("blur", (event)=>{
+    validarNacimiento(event.target);
+})
+
+function validarNacimiento(inputNacimiento){
+    const fechaCliente=new Date(inputNacimiento.value);
+    let mensaje="";
+    if(!mayorEdad(fechaCliente)){
+        mensaje="Debes tener al menos 18 años de edad."
+    }
+    inputNacimiento.setCustomValidity(mensaje);
+}
+
+function mayorEdad(fecha){
+    const fechaActual=new Date();
+    const diferenciaFechas= new Date(fecha.getUTCFullYear()+18, fecha.getUTCMonth(), fecha.getUTCDate());
+    return fechaActual>=diferenciaFechas;
+
+}
